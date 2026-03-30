c0920ae7b471759261e8da204956450683052d57
WuMin4 <136958923+WuMin4@users.noreply.github.com>
2026-03-31 01:54:41 +0800

    Delete src directory

--- src/lib/socket.ts
+++
@@ -1,8 +0,0 @@
-import { io } from "socket.io-client";
-
-// In development, connect to the same host. In production, it will be the same origin.
-const URL = process.env.NODE_ENV === "production" ? undefined : "/";
-
-export const socket = io(URL as string, {
-  autoConnect: false,
-});
